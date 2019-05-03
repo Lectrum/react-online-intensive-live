9d75dc48106d37feb2dbe7afa4594ff67dd2b969
Dima Vakatsiienko <dvakatsiienko@lectrum.io>
2019-05-03 20:07:02 +0300

    quick update

--- src/index.js
+++ src/index.js
@@ -10,3 +10,15 @@ import { App } from './pages/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
+const c = 1;
+
+const pure = (a, b) => {
+    // Лексическое окружение
+
+    // window.localStorage.getItem('test'); // сайд-эффект
+
+    return a + b + c;
+};
+pure(1, 2);
+
+// pure или impure
